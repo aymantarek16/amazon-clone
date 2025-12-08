17bd8ad2db62e9aca90d394b4d48d37042ba7669
aymantarek16 <aymantarekm16@gmail.com>
2025-12-08 02:49:19 +0200

    fix login issue

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -1,55 +1,55 @@
-import Link from "next/link";
+import { signIn } from "@/auth";
 import { BiCaretDown } from "react-icons/bi";
 
 const SignInButton = () => {
   return (
-    <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
-      <Link href="/login" className="text-left text-white font-semibold md:text-gray-100 md:font-normal">
+    <form
+      action={async () => {
+        "use server";
+        // await signIn();
+        await signIn("google", { redirectTo: "/" });
+      }}
+      className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]"
+    >
+      <button
+       className="text-left text-white font-semibold md:text-gray-100 md:font-normal">
         Hello, sign in
-      </Link>
+      </button>
 
-      <Link
-        href="/login"
+      <button
+        type="submit"
         className="text-white font-bold hidden md:flex items-center"
       >
-        Account & Lists <span><BiCaretDown /></span>
-      </Link>
-    </div>
+        Account & Lists{" "}
+        <span>
+          <BiCaretDown />
+        </span>
+      </button>
+    </form>
   );
 };
 
 export default SignInButton;
 
-
-// import { signIn } from "@/auth";
+// import Link from "next/link";
 // import { BiCaretDown } from "react-icons/bi";
 
 // const SignInButton = () => {
 //   return (
-//     <form
-//       action={async () => {
-//         "use server";
-//         await signIn();
-//         // await signIn("google", { redirectTo: "/" });
-//       }}
-//       className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]"
-//     >
-//       <button
-//        className="text-left text-white font-semibold md:text-gray-100 md:font-normal">
+//     <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
+//       <Link href="/login" className="text-left text-white font-semibold md:text-gray-100 md:font-normal">
 //         Hello, sign in
-//       </button>
+//       </Link>
 
-//       <button
-//         type="submit"
+//       <Link
+//         href="/login"
 //         className="text-white font-bold hidden md:flex items-center"
 //       >
-//         Account & Lists{" "}
-//         <span>
-//           <BiCaretDown />
-//         </span>
-//       </button>
-//     </form>
+//         Account & Lists <span><BiCaretDown /></span>
+//       </Link>
+//     </div>
 //   );
 // };
 
 // export default SignInButton;
+
